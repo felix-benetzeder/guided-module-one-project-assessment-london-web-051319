e1c9562a8f0d54592aefd298198e8722e5c98c86
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-28 16:59:29 +0100

    final MVP

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,17 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.login(username:)
+  def self.logIn(username:)
     if User.all.map(&:username).include?(username)
       puts "Login successful"
     else
-      puts "This user does not exist"
+      puts "This user does not exist, please enter a valid username"
+      welcome
     end
+  end
+
+  def self.findUser(username)
+    User.find_by(username: username)
   end
 
   def showReviews
@@ -32,13 +37,13 @@ class User < ActiveRecord::Base
 
   def createReview(description:, rating:, book:)
     #get description rating and book from CLI
-    bookTitle = Book.find_by_title(book)
-    Review.new(description: description, rating: rating, book_id: bookTitle, user_id: self.id, date: Date.today)
+    bookID = Book.find_by_title(book).id
+    Review.create(description: description, rating: rating, book_id: bookID, user_id: self.id, date: Date.today)
   end
 
 
   def reviewContent # Shows review with book title and information
-    showReviews.map { |review| puts "#{review.book.title} - Your review was: #{review.description} and you rate the book with #{review.rating}."  }
+    showReviews.map { |review| puts "#{review.book.title} - Your review was: #{review.description} and you rated the book with #{review.rating} stars."  }
   end
 
   def showReviewContent #puts it to console

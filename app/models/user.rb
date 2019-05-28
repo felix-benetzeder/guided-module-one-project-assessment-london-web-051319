0ec314dab7f25d63d9fd474aba161dce2d5e6132
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-28 13:29:18 +0100

    Added final methods for MVP

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,14 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.login(username:)
+    if User.all.map(&:username).include?(username)
+      puts "Login successful"
+    else
+      puts "This user does not exist"
+    end
+  end
+
   def showReviews
     self.reviews
   end
@@ -22,7 +30,19 @@ class User < ActiveRecord::Base
     self.all.max_by(&:countReviews)
   end
 
-  ## Create Review
-  ## Edit review - only if self.id == user.id
+  def createReview(description:, rating:, book:)
+    #get description rating and book from CLI
+    bookTitle = Book.find_by_title(book)
+    Review.new(description: description, rating: rating, book_id: bookTitle, user_id: self.id, date: Date.today)
+  end
+
+
+  def reviewContent # Shows review with book title and information
+    showReviews.map { |review| puts "#{review.book.title} - Your review was: #{review.description} and you rate the book with #{review.rating}."  }
+  end
+
+  def showReviewContent #puts it to console
+    reviewContent.each { |content| puts content  }
+  end
 
 end

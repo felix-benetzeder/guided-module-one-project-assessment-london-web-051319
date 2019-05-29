01034c3cd9809519aade1ecae70a37df76108942
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-29 11:07:24 +0100

    restructured method flow

--- lib/cli_run.rb
+++
@@ -1,7 +0,0 @@
-# require_relative "cli_menu.rb"
-# require_relative "../app/models/user.rb"
-# require_relative "../app/models/review.rb"
-# require_relative "../app/models/book.rb"
-#
-#
-# welcome

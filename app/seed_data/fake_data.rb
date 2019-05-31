0ffe8b7403994306b9640398269cd1e18ff4c078
flexkat <catherinemaritz1@gmail.com>
2019-05-31 12:52:35 +0100

    more fake data

--- app/seed_data/fake_data.rb
+++ app/seed_data/fake_data.rb
@@ -3,7 +3,7 @@ def generate_data
     User.create(username: Faker::Creature::Cat.unique.name, full_name: Faker::Name.name)
     Book.create(title: Faker::Book.unique.title, author: Faker::Book.author, genre: Faker::Book.genre, pages: rand(50..350))
   end
-  for x in 1..60
+  for x in 1..80
     Review.create(description: Faker::Quote.famous_last_words, rating: rand(1..5), book_id:rand(1..25), user_id: rand(1..25), date: Faker::Date.backward(rand(1..500)))
   end
 end

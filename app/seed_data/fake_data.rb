b5e2a462de25d879658d7f115b27eee3c5e498f6
flexkat <catherinemaritz1@gmail.com>
2019-05-31 12:50:04 +0100

    unique book names

--- app/seed_data/fake_data.rb
+++ app/seed_data/fake_data.rb
@@ -1,7 +1,7 @@
 def generate_data
   for x in 1..25
     User.create(username: Faker::Creature::Cat.unique.name, full_name: Faker::Name.name)
-    Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, pages: rand(50..350))
+    Book.create(title: Faker::Book.unique.title, author: Faker::Book.author, genre: Faker::Book.genre, pages: rand(50..350))
   end
   for x in 1..60
     Review.create(description: Faker::Quote.famous_last_words, rating: rand(1..5), book_id:rand(1..25), user_id: rand(1..25), date: Faker::Date.backward(rand(1..500)))
